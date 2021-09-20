68f3ccd44cae554a2dd850bc0d1f6e5d14b9c013
Denis Vivdenko <denis.vivdenko.freelancer@gmail.com>
2021-09-20 13:27:34 +0300

    add PrivateKey and PublicKey generator to client part

--- Chat/client.py
+++ Chat/client.py
@@ -2,7 +2,9 @@ from twisted.internet.protocol import DatagramProtocol
 from twisted.internet import reactor 
 from blockchain.blockchain import Blockchain
 from chat.user_address import UserAddress
+from Crypto.PublicKey import RSA
 from typing import Tuple
+import os
 
 class Client(DatagramProtocol):
 
@@ -10,17 +12,32 @@ class Client(DatagramProtocol):
     def __init__(self, host, port, server_ip=None, server_port=None):
 
         # client/server settings
-        self.host: str = host
-        self.port: int = port
+        self.host: str = host # client's ip address
+        self.port: int = port # client's port
+
+        # list of online users which client will recieve in porcess of connecting to the network
+        # it will use it to communicate with other nodes in the network
         self.online_users: list = []
-        self.server_ip: str = server_ip
+        
+        self.server_ip: str = server_ip # ip/port of a server which connects users to each other 
         self.server_port: int = server_port
         self.is_server = False
 
-        if (server_ip == None) | (server_port == None):
+        # private / public keys init
+        self.USER_KEYS_FILE = '' # file with user's keys
+        self.PRIVATE_KEY = ''
+        self.PUBLIC_KEY = ''
+
+        if os.path.exists(self.USER_KEYS_FILE): # if file with keys already exists
+            pass # read from JSON file
+        else: # if not => generate new keys
+            self.PUBLIC_KEY, self.PRIVATE_KEY = self.generate_keys()
+
+        # ip and port of a server is not defined => it's server
+        if (server_ip == None) | (server_port == None): 
             self.is_server = True
         
-        if not self.is_server:
+        if not self.is_server: # if it's not a server -> sends request to connect to the network
             self.connect_to_network((self.server_ip, self.server_port))
 
     def startProtocol(self):
@@ -72,9 +89,20 @@ class Client(DatagramProtocol):
         data = '\n'.join(lines[1:])
         return data
 
+    def generate_keys(self) -> Tuple[str, str]:
+        """
+        Generates two keys for asymmetric encryption
+        :return: public_key, private_key
+        """
+        keys_length = 1024
+        keys = RSA.generate(keys_length)
+        private_key = ''.join(keys.export_key().decode().split('\n')[1:-1])
+        public_key = ''.join(keys.publickey().export_key().decode().split('\n')[1:-1])
+        return public_key, private_key
+
 
 if __name__ == '__main__':
     user_address = UserAddress().get_result()
-    new_client = Client('192.168.31.67', user_address[1], '192.168.31.67', 8888)
+    new_client = Client('127.0.0.1', user_address[1], '127.0.0.1', 8888)
     reactor.listenUDP(user_address[1], new_client)
     reactor.run()
